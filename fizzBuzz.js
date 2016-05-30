6c390df9493b7abcc35695ac4248a094fd1b6f0a
Raúl Hernández <rhernandez@idealista.com>
2016-05-30 10:23:12 +0200

    Added fizzBuzz.js and modifications in triangle.js

---
+++ fizzBuzz.js
@@ -0,0 +1,8 @@
+var count = 1
+while (count < 100){
+  if(count % 3 == 0 && count % 5 == 0) console.log("FizzBuzz");
+  else if(count % 3 == 0) console.log("Fizz");
+  else if(count % 5 == 0) console.log("Buzz");
+  else console.log(count);
+  count++;
+}
--- triangle.js
+++ triangle.js
@@ -1,6 +1,15 @@
-var whatIWantToPrint = "#";
-var b = whatIWantToPrint;
-while (b.length < 8){
-  console.log(b);
-  b = b + whatIWantToPrint;
-};
+//Write a loop that makes seven calls to console.log to output the following triangle
+//#
+//##
+//####
+//#####
+//######
+//#######
+//########
+function oneMore(whatIWantToPrint, timesToPrint){
+  var b = whatIWantToPrint;
+  while (b.length <= timesToPrint){
+    console.log(b);
+    b = b + whatIWantToPrint;
+  };
+}
