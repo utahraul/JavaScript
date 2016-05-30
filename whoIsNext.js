6cdad446cda7f939e17ad9b596a6ccafb97ef104
Raúl Hernández <rhernandez@idealista.com>
2016-05-30 09:17:19 +0200

    Prueba

---
+++ whoIsNext.js
@@ -0,0 +1,7 @@
+var whoIsNext = function(names[], number){
+  for(i=1; queue.length < number; i++){
+    var queue = queue + (names*i)
+  }
+  console.log(queue[number])
+};
+whoIsNext(["Sheldon","Leonard","Penny","Rajesh","Howard"],52);
