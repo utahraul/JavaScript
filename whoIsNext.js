7275aadb87be894edb944de683735a0656a60a37
Raúl Hernández <rhernandez@idealista.com>
2016-06-02 18:22:37 +0200

    Updated

--- whoIsNext.js
+++ whoIsNext.js
@@ -1,6 +1,10 @@
+//Sheldon, Leonard,Penny, Rajesh y Howard llegan a una máquina de bebidas, se ponen en cola y cada vez que uno bebe una bebida de la máquina, se clona y los dos clones se ponen a la cola al final
+// http://codeforces.com/problemset/problem/82/A
+
 var whoIsNext = function(names, number){
+  var queue = names;
   for(i=1; queue.length < number; i++){
-    var queue = queue + (names*i)
+    queue = queue + (names*i)
   }
   console.log(queue[number])
 };
