3ba4850ad21056fd4494093206b87d22d316fc7b
Raúl Hernández <rhernandez@idealista.com>
2016-06-13 11:25:27 +0200

    chessBoard.js en curso

--- chessBoard.js
+++ chessBoard.js
@@ -12,28 +12,28 @@ Passing this string to console.log should show something like this:
 # # # #
 
 When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
-*/
+ */
 
 var size = 8;
+var string= "";
 for (var i = 1; i <= size; i++) {
     // linea impar
     if (i % 2 != 0) {
-        for (var j = 1; j <= size; j++) {
-            if (j % 2 != 0) {
-                console.log(" ");
-            } else {
-                console.log("#");
-            }
-        }
-    } 
+      for (var j = 1; j <= size; j++) {
+        if (j % 2 != 0) {string = string + " "}
+        else {string = string + "#"}
+      };
+      string = string + "\n";
+    }
     //linea par
     else {
+      if (i % 2 != 0) {
         for (var k = 1; k <= size; k++) {
-            if (k % 2 != 0) {
-                console.log("#")
-            } else {
-                console.log(" ")
-            }
-        }
+          if (k % 2 != 0) {string = string + "#"}
+          else {string = string + " "}
+        };
+      string = string + "\n";
+    }
     }
 }
+console.log(string);
