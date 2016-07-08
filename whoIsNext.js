a5c8a86b0d83d00203e9fcedea7903b5d8c22608
Raúl Hernández <rhernandez@idealista.com>
2016-07-08 09:43:42 +0200

    Updated

--- whoIsNext.js
+++ whoIsNext.js
@@ -3,12 +3,13 @@
 // http://codeforces.com/problemset/problem/82/A
 
 var whoIsNext = function (names, placeInQueue) {
-  var queue = names;  for(i=0; queue.length < placeInQueue; i ++){
-   queue.push(queue[i]);
-   queue.push(queue[i]);
+  var queue = names;
+  for(i=0; queue.length < placeInQueue; i ++){
+    queue.push(queue[i]);
+    queue.push(queue[i]);
   }
   console.log(queue);
-  console.log("En el número " + placeInQueue + " está: " + queue[i]);
+  console.log("Si Sheldon, Leonard,Penny, Rajesh y Howard llegan a una máquina de bebidas, se ponen en cola y cada vez que uno bebe una bebida de la máquina, se clona y los dos clones se ponen a la cola al final, en el número " + placeInQueue + " está: " + queue[i-1]);
 }
 
 whoIsNext(["Sheldon","Leonard","Penny","Rajesh","Howard"],52);
