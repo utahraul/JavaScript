f61aac6cebe3b851ffd02f46b72c9bcbf55a7e02
Raúl Hernández <rhernandez@idealista.com>
2016-11-23 17:23:03 +0100

    Añadido min.js

---
+++ min.js
@@ -0,0 +1,16 @@
+var min = function(valueA, valueB) {
+    var minValue = "Son iguales";
+    if (valueA < valueB) {
+        minValue = valueA;
+        return (minValue)
+    }
+    else if (valueA > valueB) {
+    minValue = valueB;
+    return (minValue)
+    }
+    else {
+    return(minValue)
+    }
+    console.log (minValue);
+};
+min (2,1);
