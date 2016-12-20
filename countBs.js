70780ddcdf768aa14fc0a3654a7e54c78116661b
Raúl Hernández <rhernandez@idealista.com>
2016-12-20 18:20:06 +0100

    Added countChar.js

--- countBs.js
+++ countBs.js
@@ -1,3 +1,5 @@
+//Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters are in the string.
+
 var countBs = function(stringWithBs){
 var bs = 0;
  for(i =1; i <= stringWithBs.length; i ++){ var currentLetter = stringWithBs.charAt(i)
---
+++ countChar.js
@@ -0,0 +1,12 @@
+//Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+
+var countChar = function(stringWithCharacters, character){
+var characters = 0;
+ for(i =1; i <= stringWithCharacters.length; i ++){ var currentLetter = stringWithCharacters.charAt(i)
+  if (currentLetter === character) {
+ characters = characters + 1;
+}
+}
+console.log(characters);
+};
+countChar("bsdbfjkfjjdBBBjjfjjkdk fkBBBjddjdB", "B");
