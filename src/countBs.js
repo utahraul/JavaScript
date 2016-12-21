6b97cec9cfa5b0e4afdccc1a176c92ea7d9ec36b
Raúl <raul.hernandez@testeandosoftware.com>
2016-12-21 15:30:58 +0000

    Refactor fizzBuzz.js

--- src/countBs.js
+++ src/countBs.js
@@ -1,13 +1,14 @@
 //Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters are in the string.
 
-var countBs = function(stringWithBs){
-var bs = 0;
- for(var i = 0; i <= stringWithBs.length; i ++){ var currentLetter = stringWithBs.charAt(i)
+var countBs = function(stringWithBs) {
+ var bs = 0;
+ for (var i = 0; i <= stringWithBs.length; i++) {
+  var currentLetter = stringWithBs.charAt(i)
   if (currentLetter === "B") {
-bs = bs + 1;
-}
-}
-return(bs);
+   bs = bs + 1;
+  }
+ }
+ return (bs);
 };
 
 module.exports = countBs;
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,8 +1,16 @@
 var count = 1
-while (count < 100){
-  if(count % 3 == 0 && count % 5 == 0) console.log("FizzBuzz");
-  else if(count % 3 == 0) console.log("Fizz");
-  else if(count % 5 == 0) console.log("Buzz");
-  else console.log(count);
+while (count < 100) {
+  if (count % 3 == 0 && count % 5 == 0) {
+    console.log("FizzBuzz");
+  }
+  else if (count % 3 == 0) {
+    console.log("Fizz");
+  }
+  else if (count % 5 == 0) {
+    console.log("Buzz");
+  }
+  else {
+    console.log(count);
+  }
   count++;
 }
