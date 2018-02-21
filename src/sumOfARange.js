d5f8442ad9cb46b75aa3bae25ab3e76958cd90c7
Raúl Hernández <rhernandez@idealista.com>
2018-02-21 11:01:28 +0100

    Tests no pasan para sumOfARange.js

--- src/sumOfARange.js
+++ src/sumOfARange.js
@@ -7,11 +7,18 @@ var createArray = function(start, end) {
 };
 
 var sum = function(arrayOfNumbers) {
-  for (a = 0; a <= arrayOfNumbers.length - 1; a++) {
-    console.log(arrayOfNumbers[a]);
+  var total = 0;
+  var numberToAdd;
+  for (var a = 0; a <= arrayOfNumbers.length - 1; a++) {
+    numberToAdd = arrayOfNumbers[a];
+    total = total + numberToAdd;
   }
+  //return(total);
+  console.log(total);
 };
 
-var sumOfARange = function(start, end){
-    sum(createArray(start, end));
-}
+var sumOfARange = function(start, end) {
+  sum(createArray(start, end));
+};
+
+module.exports = sumOfARange;
--- test/sumOfARange-test.js
+++ test/sumOfARange-test.js
@@ -1,7 +1,7 @@
 //test/sumOfARange-test.js
 var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
-var countChar = require('./../src/sumOfARange.js');
+var sumOfARange = require('./../src/sumOfARange.js');
 
 describe('sumOfARange', function() {
   it('sumOfARange() should return 55 if the values 1 and 10 are passed in', function() {
@@ -9,3 +9,11 @@ describe('sumOfARange', function() {
     expect(sumOf1to10Range).to.equal(55);
     });
 });
+
+describe('sumOfARange', function() {
+    it('sumOfARange() should return 6 if the values 1 and 3 are passed in', function() {
+      var sumOf1to10Range = sumOfARange(1, 3);
+      expect(sumOf1to10Range).to.equal(6);
+      });
+  });
+  
