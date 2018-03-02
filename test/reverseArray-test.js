8acbfda18027970b02a54d06ed27c7d5ad90bfa3
utahraul <rhernandez@testeandosoftware.com>
2018-03-02 11:55:17 +0100

    Añadido código para pasar el test, pero aún no pasa

--- test/reverseArray-test.js
+++ test/reverseArray-test.js
@@ -4,8 +4,8 @@ var expect = chai.expect; // we are using the "expect" style of Chai
 var reverseArray = require("./../src/reverseArray.js");
 
 describe("reverseArray", function() {
-  it("reverseArray() should return [C,B,A] if the array [A,B,C] passed in", function() {
-    var arrayReversed = reverseArray([]);
-    expect(arrayReversed).to.equal([]);
+  it("reverseArray() should return [C,B,A] if the array [A,B,C] is passed in", function() {
+    var arrayReversed = reverseArray(['A','B','C']);
+    expect(arrayReversed).to.equal(['C','B','A']);
   });
 });
