bee44a5c53aa522bbf8db161cf3822b7448b884c
utahraul <rhernandez@testeandosoftware.com>
2018-03-02 12:28:19 +0100

    Corregidos errores reportados por Sonarcloud

--- src/reverseArray.js
+++ src/reverseArray.js
@@ -1,7 +1,7 @@
 var reverseArray = function(arrayToReverse){
     var reversedArray = [];
-    for(i=(arrayToReverse.length)-1; i>=0; i--){
-        itemToAdd = arrayToReverse[i];
+    for(var i=(arrayToReverse.length)-1; i>=0; i--){
+        var itemToAdd = arrayToReverse[i];
         reversedArray.push(itemToAdd);
     }
     console.log(reversedArray);
